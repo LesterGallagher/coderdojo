bfea9e7fecd9727b35081fa96526cc4332141878
LesterGallagher <LesterGallagher@gmail.com>
2019-05-19 23:47:31 +0200

    various

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
 import notFound from '../uploads/error-2129569_1920.jpg';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class NotFoundPage extends React.Component {
   render() {
     return (
       <Layout location={this.props.location}>
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-        <img alt="Not found" src={notFound} />
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <h1>Not Found</h1>
+              <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+              <img height={300} alt="Not found" src={notFound} />
+            </Col>
+          </Row>
+        </Grid>
       </Layout>
     )
   }
